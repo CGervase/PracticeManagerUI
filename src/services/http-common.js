b97ff056180160e193d940dcd91bfe1ba219458c
Charles Gervase <cgervase92@gmail.com>
2019-08-27 22:49:12 -0400

    Added prod .env file, and changes to handle dev and prod environment variables with the same line of code

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-    baseURL: `${process.env.VUE_APP_API_BASE_URI}:${process.env.VUE_APP_API_PORT}`
+    baseURL: `${process.env.VUE_APP_API_BASE_URI}${process.env.VUE_APP_API_PORT}`
 })
